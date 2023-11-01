58a29a5673ae97eed014365dc00b3e169a4a0811
sandar234 <132479416+sandar234@users.noreply.github.com>
2023-11-01 03:03:41 +0200

    setting modified

--- online_store/settings.py
+++ online_store/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-dkdd&p&sn_w&@sgqjjeetu_jeclgnhngn(v92_46&rygzy5tor
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 # Application definition
 
