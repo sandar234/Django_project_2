03967b5c71683e5c1cb95fadb4c6e4a573c24065
sandar234 <132479416+sandar234@users.noreply.github.com>
2023-11-01 02:31:47 +0200

    modify static

--- online_store/settings.py
+++ online_store/settings.py
@@ -13,6 +13,8 @@ import os
 from pathlib import Path
 
 from django.contrib import staticfiles
+from django.contrib.staticfiles.storage import StaticFilesStorage
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -119,13 +121,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 # STATICFILES_DIRS = (os.path.join(BASE_DIR), 'static')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
-MEDIA_URL = 'media/'
+
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
