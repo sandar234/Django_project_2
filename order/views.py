a3815f84de6021ee2ade9f6a6e36249b1dfd6bdd
sandar234 <132479416+sandar234@users.noreply.github.com>
2023-11-09 21:35:52 +0200

    dgbfjf

--- order/views.py
+++ order/views.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from pprint import pprint
 from random import randint
+from urllib import request
 
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
@@ -44,17 +45,21 @@ def add_to_cart(request, pk):
 
     return redirect('products-by-category', pk)
 
+
 @login_required()
 def increase_quantity(request, pk):
-    cart_product = get_object_or_404(OrderCart, product_id=pk, user_id=request.user.id)
+    cart_product = get_object_or_404(OrderCart, product_id=pk,
+                                     user_id=request.user.id)
     cart_product.quantity += 1
     cart_product.amount = cart_product.product.price * cart_product.quantity
     cart_product.save()
     return redirect('cart-list')
 
+
 @login_required()
 def decrease_quantity(request, pk):
-    cart_product = get_object_or_404(OrderCart, product_id=pk, user_id=request.user.id)
+    cart_product = get_object_or_404(OrderCart, product_id=pk,
+                                     user_id=request.user.id)
     if cart_product.quantity > 1:
         cart_product.quantity -= 1
         cart_product.amount = cart_product.product.price * cart_product.quantity
@@ -64,14 +69,14 @@ def decrease_quantity(request, pk):
     return redirect('cart-list')
 
 
-
 class CartListView(ListView):
     template_name = 'order/cart_list.html'
     model = OrderCart
     context_object_name = 'cart_products'
 
     def get_queryset(self):
-        result = (OrderCart.objects.filter(cart_item=1, user_id=self.request.user.id))
+        result = (
+            OrderCart.objects.filter(cart_item=1, user_id=self.request.user.id))
         return result
 
     def get_context_data(self, **kwargs):
@@ -83,9 +88,7 @@ class CartListView(ListView):
         return context
 
 
-
 def add_to_wishlist(request, pk):
-
     # verificam daca produsul este in cosul de cumparaturi
     if OrderCart.objects.filter(user_id=request.user.id,
                                 product_id=pk).exists():
@@ -101,7 +104,7 @@ def add_to_wishlist(request, pk):
             cart_item=False,
             wishlist_item=True,
             quantity=1,
-            amount = Product.objects.get(id=pk).price,
+            amount=Product.objects.get(id=pk).price,
             created_at=datetime.now()
         )
     messages.success(request, f"Product added to the wishlist successfully.")
@@ -114,15 +117,17 @@ class ListViewWishList(ListView):
     context_object_name = 'wish_lists'
 
     def get_queryset(self):
-        result =  (OrderCart.objects.filter(wishlist_item=1, user_id=self.request.user.id))
+        result = (OrderCart.objects.filter(wishlist_item=1,
+                                           user_id=self.request.user.id))
         return result
-
 
 
 @login_required()
 def delete_from_cart(request, pk):
-    if OrderCart.objects.filter(user_id=request.user.id, id =pk, wishlist_item=1).exists():
-        OrderCart.objects.filter(user_id=request.user.id, id=pk).update(cart_item=0)
+    if OrderCart.objects.filter(user_id=request.user.id, id=pk,
+                                wishlist_item=1).exists():
+        OrderCart.objects.filter(user_id=request.user.id, id=pk).update(
+            cart_item=0)
         return redirect('cart-list')
     else:
         OrderCart.objects.filter(user_id=request.user.id, id=pk).delete()
@@ -131,8 +136,10 @@ def delete_from_cart(request, pk):
 
 @login_required()
 def delete_from_wishlist(request, pk):
-    if OrderCart.objects.filter(user_id=request.user.id, id =pk, cart_item=1).exists():
-        OrderCart.objects.filter(user_id=request.user.id, id=pk).update(wishlist_item=0)
+    if OrderCart.objects.filter(user_id=request.user.id, id=pk,
+                                cart_item=1).exists():
+        OrderCart.objects.filter(user_id=request.user.id, id=pk).update(
+            wishlist_item=0)
         return redirect('wish-list')
     else:
         OrderCart.objects.filter(user_id=request.user.id, id=pk).delete()
@@ -141,17 +148,18 @@ def delete_from_wishlist(request, pk):
 
 @login_required()
 def move_favorites_to_cart(request, pk):
-
     current_order = OrderCart.objects.get(user_id=request.user.id, id=pk)
     amount = current_order.quantity * current_order.product.price
-    OrderCart.objects.filter(user_id=request.user.id, id=pk).update(cart_item=1, wishlist_item=1, amount=amount)
+    OrderCart.objects.filter(user_id=request.user.id, id=pk).update(cart_item=1,
+                                                                    wishlist_item=1,
+                                                                    amount=amount)
     messages.success(request, f"Product added to the cart successfully.")
 
     return redirect('cart-list')
 
 
 class PlaceOrderCreateView(LoginRequiredMixin, CreateView):
-    template_name =  'order/place_order.html'
+    template_name = 'order/place_order.html'
     model = PlaceOrder
     form_class = PlaceOrderForm
     success_url = reverse_lazy('home_page')
