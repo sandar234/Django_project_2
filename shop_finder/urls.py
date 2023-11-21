55971404b320428f6de0fab06dac13cb63ce0edd
sandar234 <132479416+sandar234@users.noreply.github.com>
2023-11-21 22:57:58 +0200

    shop finder

--- shop_finder/urls.py
+++ shop_finder/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from shop_finder import views
 
+
 urlpatterns = [
-    path('shop_finder', views.ShopFinderTemplateView.as_view(), name='shop-finder'),
+    path('shop_finder', views.ShopFinderTemplateView.as_view(), name='shop-fi'),
 ]
