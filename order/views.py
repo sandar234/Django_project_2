71d52906009182cd03aa8fb6412b62cc1eee318c
sandar234 <132479416+sandar234@users.noreply.github.com>
2023-11-27 16:25:02 +0200

    ffff

--- order/views.py
+++ order/views.py
@@ -70,16 +70,35 @@ def decrease_quantity(request, pk):
 
 
 class CartListView(ListView):
+    """
+        The CartListView class represents a view for the shopping cart.
+
+        Class Attributes:
+        - template_name: The HTML template name for displaying the list.
+        - model: The data model associated with this view (OrderCart).
+        - context_object_name: The name through which objects in the list are accessed in the template.
+
+        Methods:
+        - get_queryset: Returns a filtered queryset of OrderCart objects based on cart_item and user_id.
+        - get_context_data: Calculates the total amounts in the shopping cart and adds it to the view's context.
+        """
     template_name = 'order/cart_list.html'
     model = OrderCart
     context_object_name = 'cart_products'
 
     def get_queryset(self):
+        """
+                Returns a filtered queryset of OrderCart objects.
+                Filtering is based on cart_item and user_id.
+                """
         result = (
             OrderCart.objects.filter(cart_item=1, user_id=self.request.user.id))
         return result
 
     def get_context_data(self, **kwargs):
+        """
+                Calculates the total amounts in the shopping cart and adds it to the view's context.
+                """
         context = super().get_context_data(**kwargs)
         total_amount = 0
         for cart_product in context['cart_products']:
@@ -89,6 +108,24 @@ class CartListView(ListView):
 
 
 def add_to_wishlist(request, pk):
+    """
+       View function to add a product to the user's wishlist.
+
+       Parameters:
+       - request: The HTTP request object.
+       - pk: The primary key of the product to be added to the wishlist.
+
+       Logic:
+       - Check if the product is already in the shopping cart.
+       - If yes, update the existing entry to mark it as a wishlist item.
+       - If not, create a new entry in the shopping cart with wishlist_item set to True.
+
+       Messages:
+       - Display a success message after adding the product to the wishlist.
+
+       Redirect:
+       - Redirect the user to the 'products-by-category' view for the specified category after the operation.
+       """
     # verificam daca produsul este in cosul de cumparaturi
     if OrderCart.objects.filter(user_id=request.user.id,
                                 product_id=pk).exists():
@@ -148,6 +185,22 @@ def delete_from_wishlist(request, pk):
 
 @login_required()
 def move_favorites_to_cart(request, pk):
+    """
+        View function to move a product from the wishlist to the shopping cart.
+
+        Parameters:
+        - request: The HTTP request object.
+        - pk: The primary key of the wishlist item to be moved to the cart.
+
+        Logic:
+        - Retrieve the current wishlist item based on the provided primary key.
+        - Calculate the total amount for the quantity of the product.
+        - Update the current wishlist item to mark it as a cart item and update the amount.
+        - Display a success message after moving the product to the cart.
+
+        Redirect:
+        - Redirect the user to the 'cart-list' view after the operation.
+        """
     current_order = OrderCart.objects.get(user_id=request.user.id, id=pk)
     amount = current_order.quantity * current_order.product.price
     OrderCart.objects.filter(user_id=request.user.id, id=pk).update(cart_item=1,
